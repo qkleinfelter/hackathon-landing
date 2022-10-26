cb35223a6900226adc9b506e6caf8fa121ff1baa
RobertConnolly <rec73@uakron.edu>
2022-10-26 16:41:13 -0400

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
+import { UserComponent } from './pages/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'user/:id', component: UserComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: '**', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
